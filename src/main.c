1e62bb92283a0b6826fdbbbb07bfcb791b7c0b36
Wahl <fredericmarcel.wahl@exyte.net>
2026-03-23 11:59:24 +0100

    handle input/output file

--- src/main.c
+++ src/main.c
@@ -2,12 +2,24 @@
 
 int	main(int argc, char **argv, char **envp)
 {
-	(void)argv;
+	t_pipex	pipex;
+
 	(void)envp;
 	if (argc != 5)
 	{
 		write(2, "Usage: ./pipex file1 cmd1 cmd2 file2\n", 36);
 		return (1);
 	}
+	pipex.infile = open(argv[1], O_RDONLY);
+	if (pipex.infile < 0)
+		error_exit(argv[1]);
+	pipex.outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (pipex.outfile < 0)
+	{
+		close(pipex.infile);
+		error_exit(argv[4]);
+	}
+	close(pipex.infile);
+	close(pipex.outfile);
 	return (0);
 }
