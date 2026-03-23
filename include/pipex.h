8664ea6358dcd39517255a4cabf21e3b46e3d0fe
Wahl <fredericmarcel.wahl@exyte.net>
2026-03-23 10:11:13 +0100

    header, struct etc

---
+++ include/pipex.h
@@ -0,0 +1,20 @@
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+
+typedef struct s_pipex
+{
+	int		infile;
+	int		outfile;
+	int		pipe_fd[2];
+	char	**cmd_args;
+	char	*cmd_path;
+}	t_pipex;
+
+#endif
