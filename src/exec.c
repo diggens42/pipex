c22174d66cf163b597fb52af886cb2e95b722b14
diggens42 <132611571+diggens42@users.noreply.github.com>
2026-08-25 19:44:31 +0200

    get path from envp

--- src/exec.c
+++ src/exec.c
@@ -12,9 +12,34 @@
 
 #include "../include/pipex.h"
 
-static char *ft_find_path(t_px *px, char *cmd)
+static char	*ft_join_path(char *dir, char *cmd)
 {
 	
+}
+
+static char	*ft_find_path(t_px *px, char *cmd)
+{
+	char	**dir_paths;
+	char	*full_path;
+	int		i;
+
+	if (ft_strchr(cmd, '/'))
+		return (ft_strdup(cmd));
+	dir_paths = ft_split(ft_get_env_path(px->envp), ':');
+	if (!dir_paths)
+		ft_error(px, "malloc failed in ft_find_path", ERR_SYS);
+	i = 0;
+	while (dir_paths[i])
+	{
+		full_path = ft_strjoin(dir_paths[i], "/");
+		if (full_path && access(full_path, X_OK) == 0)
+			break ;
+		free(full_path);
+		full_path = NULL;
+		i++;
+	}
+	ft_free_strarray(dir_paths);
+	return (NULL);
 }
 
 void	ft_exec_cmd(t_px *px, char *cmd)
