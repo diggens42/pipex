4b40f51f60e52c2cc2b3001c492faa9cc343d4a9
diggens42 <132611571+diggens42@users.noreply.github.com>
2026-08-25 16:03:32 +0200

    exec_cmd with execve

--- src/exec.c
+++ src/exec.c
@@ -12,7 +12,23 @@
 
 #include "../include/pipex.h"
 
+static char *ft_find_path(t_px *px, char *cmd)
+{
+	
+}
+
 void	ft_exec_cmd(t_px *px, char *cmd)
 {
-
+	px->args = ft_split(cmd, ' ');
+	if (!px->args)
+		ft_error(px,"malloc failed in ft_exec_cmd", ERR_SYS);
+	if (!px->args[0])
+		ft_error(px, cmd, 127);
+	px->path = ft_find_path(px, px->args[0]);
+	if (!px->path || access(px->path, F_OK) != 0)
+		ft_error_cmd(px, px->args[0], 127);
+	if (access(px->path, X_OK) != 0)
+		ft_error_cmd(px, px->args[0], 126);
+	execve(px->path, px->args, px->envp);
+	ft_error(px, px->args[0], ERR_SYS);
 }
